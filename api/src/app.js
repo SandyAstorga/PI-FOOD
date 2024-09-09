800354200a2557621e90e69d581db62c458d5f2e
SandyAstorga <sandyastorga13@gmail.com>
2024-09-09 12:26:45 -0600

    feat:changes

--- api/src/app.js
+++ api/src/app.js
@@ -1,54 +1,8 @@
-// const express = require('express');
-// const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const routes = require('./routes/index.js');
-// const cors = require('cors');
-
-// require('./db.js');
-
-// const server = express();
-
-// server.name = 'API';
-
-// // Configura CORS
-// server.use(cors({
-//   origin: 'http://localhost:3000', // Permite solicitudes desde este origen
-//   credentials: true, // Permite el envío de cookies y otros encabezados de autenticación
-// }));
-
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://pi-food-deploy-ten.vercel.app'); // update to match the domain you will make the request from
-//   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-
-// server.use('/', routes);
-
-// // Error catching endware.
-// server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-//   const status = err.status || 500;
-//   const message = err.message || err;
-//   console.error(err);
-//   res.status(status).send(message);
-// });
-
-// module.exports = server;
-
-
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors');
 
 require('./db.js');
 
@@ -56,23 +10,23 @@ const server = express();
 
 server.name = 'API';
 
-// Configura CORS
-server.use(cors({
-  origin: ['http://localhost:3000', 'https://pi-food-deploy-ten.vercel.app'], // Permite solicitudes desde estos orígenes
-  credentials: true, // Permite el envío de cookies y otros encabezados de autenticación
-}));
-
-
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pi-food-deploy-ten.vercel.app'); // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  res.header('Access-Control-Allow-Credentials', 'true');
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
--- api/src/controllers/diets_controller.js
+++ api/src/controllers/diets_controller.js
@@ -1,49 +1,21 @@
-// const axios = require("axios");
-// const { Diets } = require("../db"); 
-// const { API_KEY } = process.env;
-
-// const getAllDiets = async (req, res) => {
-//     const DietsApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`);
-//     const types = await DietsApi.data.results.map(el => el.diets) 
-//     const typesDiets = types.flatMap(el => el) //Mapea nuevamente el resultado que es un array de arrays en un solo array con el metodo flatMap
-//         for (let i = 0; i < typesDiets.length; i++) {
-//             // console.log(typesDiets);
-//         await Diets.findOrCreate({ 
-//                 where: { name: typesDiets[i] } 
-//             })
-//         }
-//     const allDiets = await Diets.findAll();
-//     return allDiets
-// };
-
-// module.exports = {
-//     getAllDiets,
-// };
+const axios = require("axios");
+const { Diets } = require("../db"); 
+const { API_KEY } = process.env;
 
 const getAllDiets = async (req, res) => {
-    try {
-        const DietsApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`);
-        const types = DietsApi.data.results.map(el => el.diets);
-        const typesDiets = types.flatMap(el => el);
-
-        // Itera sobre las dietas
-        for (const diet of typesDiets) {
-            // Verifica si la dieta ya existe
-            const docRef = admin.firestore().collection('diets').doc(diet);
-            const doc = await docRef.get();
-
-            if (!doc.exists) {
-                // Si no existe, crea un nuevo documento
-                await docRef.set({ name: diet });
-            }
+    const DietsApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`);
+    const types = await DietsApi.data.results.map(el => el.diets) 
+    const typesDiets = types.flatMap(el => el) //Mapea nuevamente el resultado que es un array de arrays en un solo array con el metodo flatMap
+        for (let i = 0; i < typesDiets.length; i++) {
+            // console.log(typesDiets);
+        await Diets.findOrCreate({ 
+                where: { name: typesDiets[i] } 
+            })
         }
+    const allDiets = await Diets.findAll();
+    return allDiets
+};
 
-        // Obtén todas las dietas para enviar en la respuesta
-        const dietsSnapshot = await admin.firestore().collection('diets').get();
-        const allDiets = dietsSnapshot.docs.map(doc => doc.data());
-        return res.status(200).json(allDiets);
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send('Error al obtener las dietas');
-    }
+module.exports = {
+    getAllDiets,
 };
